2e6c727fa71753c17096a721fa3f9eebc69390c6
MrEritra <mstick2007@gmail.com>
2023-11-24 23:10:27 +0100

    Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
+#include <SFML/Window.hpp>
 #include <iostream>
-int main() {
-    std::cout << "Hi" << std::endl;
+int main()
+{
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
     return 0;
 }
