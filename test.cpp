0d1b67494985351888f0c6a433f7eb56d48352fe
alex <alexander.schicktanz@web.de>
2023-11-25 01:18:11 +0100

    Nichts

--- test.cpp
+++ test.cpp
@@ -1,9 +1,32 @@
 #include <cmath>
 #include "point.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+//Setup
+#define nums 100
+Point points[nums];
+void setup(){
+    for(short i = 0; i < nums; i++){
+        points[i].coords = {(double)i,(double)i};
+    }
+}
+
+void iterate(){
+    for(short i = 0; i < nums; i++){
+        for(short j = 0; j < nums; j++){
+            if(j==i){
+                continue;
+            }
+            points[i].attract(points[j]);
+        }
+    }
+    for(short i = 0; i < nums; i++){
+        points[i].move();
+    }
+}
 int main(){
-    Point* p1 = new Point(1,2,{1,1});
-    Point* p2 = new Point(1,1,{4,1});
-    Point points[2];
-    std::cout << ((p1->checkColl(*p2))?"True":"False") << std::endl;
+    setup();
+    std::cout << ((points[0].checkColl(points[1]))?"True":"False") << std::endl;
 }
