1e18ac561262b1907c7413de089be1545d7aea94
Jake <Jake@LAPTOP-CST25TI8>
2023-11-25 01:01:51 +0100

    draw circle centered

--- src/draw.cpp
+++ src/draw.cpp
@@ -1,7 +1,8 @@
 #include "draw.h"
 
 void draw(Vec2 position, double radius, sf::RenderWindow* window) {
-    sf::CircleShape shape(50);
+    sf::CircleShape shape(radius);
     shape.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+    shape.setPosition(position.x-radius,position.y-radius);
     window->draw(shape);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include "defines.h"
 #include <iostream>
 #include "draw.h"
+#define WINDOWHEIGHT 600
+#define WINDOWWIDTH 800
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-
+    sf::RenderWindow window(sf::VideoMode(WINDOWWIDTH, WINDOWHEIGHT), "My window");
+    Vec2 pos = {0,0};
     // run the program as long as the window is open
     while (window.isOpen())
     {
         window.clear();
         // check all the window's events that were triggered since the last iteration of the loop
-        Vec2 pos = {0,0};
-        draw(pos,0,&window);
+        
+        draw(pos,50,&window);
+        //pos.x += 0.1;
+        //pos.y += 0.1;
         sf::Event event;
         window.display();
         while (window.pollEvent(event))
